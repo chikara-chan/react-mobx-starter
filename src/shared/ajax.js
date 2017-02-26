1d87e4446a1ab7afcebb7a436235879a6037f3be
chikara <chenhongtu@51xianqu.net>
2017-02-26 20:50:18 +0800

    style: lint

--- src/shared/ajax.js
+++ src/shared/ajax.js
@@ -1,18 +1,25 @@
 import request from 'superagent'
+import {message} from 'antd'
+
+message.config({
+  top: 63
+})
 
 /**
- * @param  {Object} options
+ * @param {Object} options
  * @return {Object} Return Promise
  */
 function ajax(options) {
   const defaults = {
     url: null,
     type: 'post',
-    data: {}
+    data: {
+    }
   }
   let promise, action
 
-  options = Object.assign({}, defaults, options)
+  options = Object.assign({
+  }, defaults, options)
   promise = request[options.type](options.url).withCredentials()
   Object.keys(options).forEach(key => {
     if (!key.match(/url|type|data/)) {
@@ -23,9 +30,14 @@ function ajax(options) {
 
   return new Promise(resolve => {
     promise[action](options.data).then(res => {
-      resolve(res.body)
-    }).catch(err => {
-      console.log(err)
+      if (!res.body.status) {
+        message.error(res.body.message)
+
+        return
+      }
+      resolve(res.body.entry)
+    }).catch(() => {
+      message.error('出错了')
     })
   })
 }
---
+++ src/shared/utils.js
@@ -0,0 +1,8 @@
+const mapOrderState = {
+  0: '待接单',
+  1: '已接单'
+}
+
+export default {
+  mapOrderState
+}
