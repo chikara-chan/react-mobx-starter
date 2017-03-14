1428aac1d18d176386d3a2c590ed31d706707166
伯方 <bofang@51xianqu.net>
2017-03-14 10:14:02 +0800

    update

--- src/commodity/list/components/HandleSection.js
+++ src/commodity/list/components/HandleSection.js
@@ -21,13 +21,16 @@ class HandleSection extends PureComponent {
 
   render() {
 
+    let selectedList = this.props.handleStore.selectedList;
     return (
       <div className={styles.handleSection}>
-        <Popconfirm placement="right" title="确认删除选中商品吗？" onConfirm={this.doHandle('delete')} okText="确认" cancelText="取消">
+        <Popconfirm placement="right" title={`确认删除已选中的 ${selectedList.length} 个商品吗？`} onConfirm={this.doHandle('delete')} okText="确认" cancelText="取消">
             <Button>删 除</Button>
         </Popconfirm>
         <Button onClick={this.doHandle('online')}>上 架</Button>
-        <Button onClick={this.doHandle('offline')}>下 架</Button>
+        <Popconfirm placement="right" title={`确认下架已选中的 ${selectedList.length} 个商品吗？`} onConfirm={this.doHandle('offline')} okText="确认" cancelText="取消">
+          <Button>下 架</Button>
+        </Popconfirm>
         <Button onClick={this.doHandle('export')}>导出店铺全部商品</Button>
       </div>
     )
